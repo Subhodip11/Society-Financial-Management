cf1c6638056f6686b160dbd36c8996fa22dc8ac7
Subhodip11 <1941012209.subhodip@gmail.com>
2022-09-26 18:15:37 +0530

    search option completed

--- virtusa-project-backend/Routes/searchSociety.js
+++ virtusa-project-backend/Routes/searchSociety.js
@@ -6,23 +6,45 @@ const registrationModel = require('../schema/SocietyRegistration');
 router.post("/searchSociety", verifier, (req, res) => {
     const { option, searchVal } = req.body;
     console.log(searchVal, option)
+        //search by society ID
     if (option === '1_1') {
         registrationModel.findOne({ societyID: Number(searchVal) }, (err, doc) => {
             if (err) {
                 return res.json({ isAuthenticated: true, data: null })
             }
-            return res.json({ isAuthenticated: true, data: doc })
+            if (doc) {
+                let temp = [];
+                temp.push(doc);
+                return res.json({ isAuthenticated: true, data: temp })
+            } else {
+                return res.json({ isAuthenticated: true, data: null })
+            }
+
 
         })
-    } else if (option === '2_1') {
+    }
+    //serach by society name
+    else if (option === '2_1') {
         registrationModel.findOne({ societyName: searchVal }, (err, doc) => {
             if (err) {
                 return res.json({ isAuthenticated: true, data: null })
             }
-            return res.json({ isAuthenticated: true, data: doc })
+            if (Array.isArray(doc))
+                return res.json({ isAuthenticated: true, data: doc })
+            else {
+                if (doc) {
+                    let temp = [];
+                    temp.push(doc);
+                    return res.json({ isAuthenticated: true, data: temp })
+                } else {
+                    return res.json({ isAuthenticated: true, data: null })
+                }
+            }
 
         })
-    } else if (option === '3_1') {
+    }
+    //search by city name
+    else if (option === '3_1') {
         registrationModel.find({ city: searchVal }, (err, doc) => {
             if (err) {
                 return res.json({ isAuthenticated: true, data: null })
@@ -30,14 +52,25 @@ router.post("/searchSociety", verifier, (req, res) => {
             return res.json({ isAuthenticated: true, data: doc })
 
         })
-    } else if (option === '4_1') {
+    }
+    //search by pincode 
+    else if (option === '4_1') {
         registrationModel.findOne({ pincode: searchVal }, (err, doc) => {
             if (err) {
                 return res.json({ isAuthenticated: true, data: null })
             }
+            if (Array.isArray(doc))
+                return res.json({ isAuthenticated: true, data: doc })
+            else {
+                if (doc) {
+                    let temp = [];
+                    temp.push(doc);
+                    return res.json({ isAuthenticated: true, data: temp })
+                } else {
+                    return res.json({ isAuthenticated: true, data: null })
+                }
 
-            return res.json({ isAuthenticated: true, data: doc })
-
+            }
         })
     }
 })
